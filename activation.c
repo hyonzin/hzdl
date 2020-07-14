2ac9cc5b3980d7758bf9478b5e740daa3125aee8
hyonzin <hyeonjin507@gmail.com>
2020-07-14 16:55:22 +0900

    activation ReLU

--- activation.c
+++ activation.c
@@ -8,3 +8,7 @@ float Sigmoid(float val) {
     return 1 / (1 + exp(-val));
 }
 
+float ReLU(float val) {
+    return fmax(val, 0);
+}
+
--- activation.h
+++ activation.h
@@ -4,4 +4,5 @@
 
 float None(float val);
 float Sigmoid(float val);
+float ReLU(float val);
 
